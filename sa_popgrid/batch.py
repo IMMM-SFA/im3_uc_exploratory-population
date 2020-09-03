42732ff20525307e860419fdfef2a9496d2116b0
crvernon <chrisrvernon@gmail.com>
2020-09-03 15:20:36 -0400

    adding batch job code

--- sa_popgrid/batch.py
+++ sa_popgrid/batch.py
@@ -6,6 +6,62 @@ import numpy as np
 from population_gravity import Model
 
 
+def main(n_samples, input_directory, sample_directory, output_directory, sample_index, end_yr, state_name, ssp):
+    """Create run outputs for each sample and problem dictionary.
+
+    :param n_samples:
+    :param input_directory:
+    :param sample_directory:
+    :param output_directory:
+    :param sample_index:
+    :param end_yr:
+    :param state_name:
+    :param ssp:
+    :return:
+    """
+
+    lhs_problem_file = os.path.join(sample_directory, f'lhs_{n_samples}_problem_dict.p')
+    lhs_sample_file = os.path.join(sample_directory, f'lhs_{n_samples}_sample.npy')
+
+    # load the sample files
+    lhs_problem = pickle.load(open(lhs_problem_file, 'rb'))
+    lhs_sample = np.load(lhs_sample_file)[sample_index]
+
+    # get the parameter name list
+    param_name_list = lhs_problem.get('names')
+
+    # run the model for the target sample
+    run = Model(grid_coordinates_file=os.path.join(input_directory, f'{state_name}_coordinates.csv'),
+                historical_rural_pop_raster=os.path.join(input_directory, f'{state_name}_rural_2010_1km.tif'),
+                historical_urban_pop_raster=os.path.join(input_directory, f'{state_name}_urban_2010_1km.tif'),
+                historical_suitability_raster=os.path.join(input_directory, f'{state_name}_mask_short_term.tif'),
+                projected_population_file=os.path.join(input_directory, f'{state_name}_{ssp}_popproj.csv'),
+                one_dimension_indices_file=os.path.join(input_directory, f'{state_name}_within_indices.txt'),
+                output_directory=output_directory,
+                alpha_urban=lhs_sample[param_name_list.index('alpha_urban')],
+                alpha_rural=lhs_sample[param_name_list.index('alpha_rural')],
+                beta_urban=lhs_sample[param_name_list.index('beta_urban')],
+                beta_rural=lhs_sample[param_name_list.index('beta_rural')],
+                kernel_distance_meters=lhs_sample[param_name_list.index('kernel_distance_meters')],
+                scenario=ssp,  # shared socioeconomic pathway abbreviation
+                state_name=state_name,
+                historic_base_year=2010,
+                projection_start_year=2020,
+                projection_end_year=end_yr,
+                time_step=10,
+                write_raster=False,
+                write_array1d=True,
+                write_array2d=False,
+                write_suitability=True,
+                write_csv=False,
+                compress_csv=True,
+                write_logfile=False,
+                run_number=sample_index,
+                output_total=False)
+
+    run.downscale()
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -23,43 +79,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    lhs_problem_file = os.path.join(args.sample_directory, f'lhs_{args.n_samples}_problem_dict.p')
-    lhs_sample_file = os.path.join(args.sample_directory, f'lhs_{args.n_samples}_sample.npy')
-
-    # load the sample files
-    lhs_problem = pickle.load(open(lhs_problem_file, 'rb'))
-    lhs_sample = np.load(lhs_sample_file)[args.sample_index]
-
-    # get the parameter name list
-    param_name_list = lhs_problem.get('names')
-
-    # run the model for the target sample
-    run = Model(grid_coordinates_file=os.path.join(args.input_directory, f'{args.state_name}_coordinates.csv'),
-                historical_rural_pop_raster=os.path.join(args.input_directory, f'{args.state_name}_rural_2010_1km.tif'),
-                historical_urban_pop_raster=os.path.join(args.input_directory, f'{args.state_name}_urban_2010_1km.tif'),
-                historical_suitability_raster=os.path.join(args.input_directory, f'{args.state_name}_mask_short_term.tif'),
-                projected_population_file=os.path.join(args.input_directory, f'{args.state_name}_{args.ssp}_popproj.csv'),
-                one_dimension_indices_file=os.path.join(args.input_directory, f'{args.state_name}_within_indices.txt'),
-                output_directory=args.output_directory,
-                alpha_urban=lhs_sample[param_name_list.index('alpha_urban')],
-                alpha_rural=lhs_sample[param_name_list.index('alpha_rural')],
-                beta_urban=lhs_sample[param_name_list.index('beta_urban')],
-                beta_rural=lhs_sample[param_name_list.index('beta_rural')],
-                kernel_distance_meters=lhs_sample[param_name_list.index('kernel_distance_meters')],
-                scenario=args.ssp,  # shared socioeconomic pathway abbreviation
-                state_name=args.state_name,
-                historic_base_year=2010,
-                projection_start_year=2020,
-                projection_end_year=args.end_yr,
-                time_step=10,
-                write_raster=False,
-                write_array1d=True,
-                write_array2d=False,
-                write_suitability=True,
-                write_csv=False,
-                compress_csv=True,
-                write_logfile=False,
-                run_number=args.sample_index,
-                output_total=False)
-
-    run.downscale()
+    main(args.n_samples,
+         args.input_directory,
+         args.sample_directory,
+         args.output_directory,
+         args.sample_index,
+         args.end_yr,
+         args.state_name,
+         args.ssp)
---
+++ sa_popgrid/slurm_batch.py
@@ -0,0 +1,69 @@
+import os
+
+
+def run_batch(output_job_script, n_samples, input_directory, sample_directory, output_directory, end_yr,
+              state_name, ssp, venv_dir, walltime='01:0:00', max_jobs=15):
+    """This script will launch SLURM tasks that will execute batch.py to create run outputs for each
+    sample and problem dictionary.
+
+    :param output_job_script:                   Full path with file name and extension to the output job script
+    :type output_job_script:                    str
+
+    :param sample_list:                         A list of samples desired.  E.g., [20, 50]
+    :type sample_list:                          list
+
+    :param output_directory:                    Full path to the directory where the outputs will be stored
+    :type output_directory:                     str
+
+    :param venv_dir:                            Full path to your Python virtual environment
+    :type venv_dir:                             str
+
+    :param walltime:                            Time limit for each SLURM job in HH:MM:SS
+    :type walltime:                             str
+
+    """
+
+    # build strings for shell variables
+    runtime_str = '{RUNTIME}'
+    sample_index = '${SLURM_ARRAY_TASK_ID}'
+
+    slurm = f"""#!/bin/sh
+                #SBATCH --partition=normal
+                #SBATCH --nodes=1
+                #SBATCH --ntasks=1
+                #SBATCH --time={walltime}
+                #SBATCH --job-name=batch
+                #SBATCH --output=slurm_batch_%a.out
+
+                # README -----------------------------------------------------------------------
+                #
+                # This script will launch SLURM tasks that will execute batch.py to create
+                # run outputs for each sample and problem dictionary.
+                #
+                # To execute this script to create a sample set of 100 with only 10 jobs 
+                # allowed at a time execute the following:
+                #
+                # `sbatch --array=0-100%15 run_batch.sh`
+                #
+                # ------------------------------------------------------------------------------
+
+                # ensure we are pointing to GDAL libs correctly
+                source ~/.bash_profile
+
+                # activate Python virtual environment
+                source {venv_dir}/bin/activate
+
+                STARTTIME=`date +%s`
+
+                # execute Python script
+                python3 -c 'from sa_popgrid.batch import main; main({n_samples}, {input_directory}, {sample_directory}, {output_directory}, {sample_index}, {end_yr}, {state_name}, {ssp})'
+
+                ENDTIME=`date +%s`
+                RUNTIME=$((ENDTIME-STARTTIME))
+
+                echo "Run completed in ${runtime_str} seconds."""
+
+    with open(output_job_script, 'w') as out:
+        out.write(slurm)
+
+    os.system(f"sbatch --array=0-{n_samples}%{max_jobs} {output_job_script}")
