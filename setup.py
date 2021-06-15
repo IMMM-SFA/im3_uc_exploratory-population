1e8955aca6eec1569e9d34be2ac6a5907d9488b5
Chris Vernon <chrisrvernon@gmail.com>
2021-06-15 06:59:06 -0400

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sa_popgrid',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='https://github.com/IMMM-SFA/sa_popgrid.git',
     license='BSD 2-Clause',
