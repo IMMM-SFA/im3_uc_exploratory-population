ee52e9f8862ac39812967d53c27436fcd3563727
Chris Vernon <chrisrvernon@gmail.com>
2021-06-15 07:53:04 -0400

    make convert_1d_array_to_csv available at package level

--- sa_popgrid/__init__.py
+++ sa_popgrid/__init__.py
@@ -2,7 +2,8 @@ from sa_popgrid.slurm_batch import submit_slurm_array
 from sa_popgrid.reproduce_experiment import reproduce_original_experiment
 from sa_popgrid.generate_inputs import build_new_data
 from sa_popgrid.run_simulation import run_simulation, run_validation, run_validation_allstates
+from sa_popgrid.utils import convert_1d_array_to_csv
 
 
 __all__ = ['submit_slurm_array', 'reproduce_original_experiment', 'build_new_data', 'run_simulation',
-           'run_validation', 'run_validation_allstates']
+           'run_validation', 'run_validation_allstates', 'convert_1d_array_to_csv']
